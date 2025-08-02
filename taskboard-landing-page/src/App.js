2289df09109bac35cfce7796a129e9a1006e7404
Mukilan R <164325463+Mukil0501@users.noreply.github.com>
2025-08-02 13:42:44 +0530

    Add files via upload

---
+++ taskboard-landing-page/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import About from "./components/About";
+import Features from "./components/Features";
+import Extras from "./components/Extras";
+import Login from "./components/Login";
+import Footer from "./components/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <About />
+      <Features />
+      <Extras />
+      <Login />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
